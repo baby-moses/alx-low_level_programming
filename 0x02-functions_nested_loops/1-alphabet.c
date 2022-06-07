c7a925f0d8eec656b2b6eaa1558b517098f3402b
baby-moses <alembimoses54@gmail.com>
2022-06-07 16:36:01 -0700

    gg

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
-#include "main.h"
-/**
- * print_alphabet - prints the alphabet in lowercase, followed by a new line.
- *
- */
-void print_alphabet(void)
+ #include "main.h"
+   /**
+   * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+  *
+  */
+   void print_alphabet(void)
 {
 	char ch;
 	ch = 'a';
