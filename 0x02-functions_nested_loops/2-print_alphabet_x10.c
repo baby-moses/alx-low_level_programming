3faf35431a153881cf72b9363384b56f4ce9946b
baby-moses <alembimoses54@gmail.com>
2022-06-07 13:21:32 -0700

    is awesome

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,47 @@
-#include "california.h"
-void print_alphabet_x10(void)
-{
-	int count = 0;
-	char letter;
+#include "main.h"
 
-	while (count++ <= 9)
+/**
+
+ * print_alphabet_x10 - prints 10 times the alphabet, in lower
+
+ * Return : Always return 0
+
+ */
+
+void print_alphabet_x10(void)
+
+{
+
+	char ch;
+
+	int i;
+
+
+
+	i = 0;
+
+
+
+	while (i < 10)
+
 	{
-		for (letter = 'a'; letter <= 'z'; letter++)
-			_putchar(letter);
+
+		ch = 'a';
+
+		while (ch <= 'z')
+
+		{
+
+			_putchar(ch);
+
+			ch++;
+
+		}
+
 		_putchar('\n');
+
+		i++;
+
 	}
+
 }
