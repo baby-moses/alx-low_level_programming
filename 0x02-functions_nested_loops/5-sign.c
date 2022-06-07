ae828aa7f52e075172d63e7980668820cf65291f
baby-moses <alembimoses54@gmail.com>
2022-06-07 13:46:45 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,15 @@
-#include "california.h"
+#include "main.h"
+/**
+ * print_sign - prints the sign of a numver
+ *
+ * @n: the int to check
+ *
+ * Return: 1 and prints + if n is greater than zero
+ *
+ * 0 and prints 0 if n is zero
+ *
+ * -1 and prints - if n is less than zero
+ */
 int print_sign(int n)
 {
 	if (n > 0)
@@ -8,12 +19,12 @@ int print_sign(int n)
 	}
 	else if (n == 0)
 	{
-		_putchar('0');
+		_putchar(48);
 		return (0);
 	}
-	else
+	else if (n < 0)
 	{
 		_putchar('-');
-		return (-1);
 	}
+	return (-1);
 }
