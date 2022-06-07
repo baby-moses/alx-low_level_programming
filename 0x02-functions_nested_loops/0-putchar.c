546b9d6599464895a24a30b24c14c7214b7d2c3f
baby-moses <alembimoses54@gmail.com>
2022-06-07 16:22:20 -0700

    mmmm

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,11 @@
 #include "main.h"
-
-
-
 /**
-
  * main - Entry point.
-
  * Description: write a program that prints "_putchar" followed by a new line.
-
  * Return: Always 0 (Success.)
-
  */
-
 int main(void)
-
 {
-
 	_putchar('_');
 
 	_putchar('p');
--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,32 +1,32 @@
-#include <stdio.h>
+ #include <stdio.h>
 
-/**
+  /**
 
- * main - finds and prints the first 98 fibonacci numbers,
+      * main - finds and prints the first 98 fibonacci numbers,
 
- * starting with 1 and 2
+             * starting with 1 and 2
 
- * followed by a new line
+                   * followed by a new line
 
- * Return: Always 0 (Success)
+                           * Return: Always 0 (Success)
 
  */
 
 int main(void)
 
-{
+  {
 
 	unsigned long int i, j, k, j1, j2, k1, k2;
 
 
 
-	j = 1;
+	        j = 1;
 
-	k = 2;
+         	k = 2;
 
 
 
-	printf("%lu", j);
+	      printf("%lu", j);
 
 
 
