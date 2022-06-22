81fdea7ae5df9e425d8e06ded5291a0769e634fd
baby-moses <alembimoses54@gmail.com>
2022-06-22 14:15:59 -0700

    She locked away a secret, deep inside herself

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,20 +1,18 @@
 #include "main.h"
 
 /**
- * _puts_recursion, Write a function that prints a string, followed by a new
- * line.
- *
- *  This is the input string
+ * _puts_recursion - function that prints a string
+ * @s: This is given
+ * Return: 0 if successul exit
  */
 
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-	{
-		_putchar('\n');
-		return;
-	}
-	_putchar(*s);
-	s++;
-	_puts_recursion(*s);
-
+if (*s == '\0')
+{
+_putchar('\n');
+return;
+}
+_putchar(*s);
+_puts_recursion(s + 1);
+}
