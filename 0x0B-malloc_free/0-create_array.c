d451297b6ba2140a4583d5aaf32c4e63ca189ad8
baby-moses <alembimoses54@gmail.com>
2022-06-28 07:09:09 -0700

    my first commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 #include <stdlib.h>
 /**
  * *create_array - creates an array of chars, and initializes it with a char
@@ -7,19 +6,20 @@
  * @c: char to be initialized with
  * Return: pointer to array or null
  **/
+
 char *create_array(unsigned int size, char c)
 {
-char *str;
-unsigned int i;
-if (size == 0)
-return (NULL);
-str = malloc(size * sizeof(char));
-if (str == NULL)
-return (NULL);
-for (i = 0; i < size; i++)
+	char *str;
+	unsigned int i;
 
-{
-str[i] = c;
-{
-return (str);
+	if (size == 0)
+		return (NULL);
+	str = malloc(size * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+	{
+		str[i] = c;
+	}
+	return (str);
 }
